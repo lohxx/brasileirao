341bece92a605f8f79b91126e2f663c6b8619f02
lohxx <lohanna.dev@gmail.com>
2019-12-04 22:52:46 -0300

    Inicializa o scrapper atraves de comando

--- championship-scrapper.js
+++ championship-scrapper.js
@@ -1,11 +1,24 @@
+#!/usr/bin/env node
+
+
+const commander = require('commander');
 const puppeteer = require('puppeteer');
 const { Classificacao } = require('./app/models');
+
+const program = new commander.Command();
+
+program
+    .command('init')
+    .description('Extrai as informações relacionadas ao campeonato brasileiro.')
+    .option('-y --year', 'ano em que o campeonato foi realizado', 2019)
+    .action(() => console.log('hello'))
+    .parse(process.argv);
 
 
 async function init() {
     const browser = await puppeteer.launch({headless: false, devtools: true});
     const page = await browser.newPage();
-    await page.goto('https://www.cbf.com.br/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-a');
+    await page.goto(`https://www.cbf.com.br/futebol-brasileiro/competicoes/campeonato-brasileiro-serie-a/${program.year}`);
 
     const tablePoints = await page.$$('table.table.m-b-20 > tbody > tr.expand-trigger');
 
@@ -28,14 +41,14 @@ async function init() {
         statistics['pontos'] = points;
         try {
             const teamAlreadyExists = await Classificacao.findAll({
-                where: {time: team, ano: 2019}});
+                where: {time: team, ano: program.year}});
 
             if(teamAlreadyExists.length) {
-                Classificacao.update(statistics, {where: {time: team, ano: 2019}});
+                Classificacao.update(statistics, {where: {time: team, ano: program.year}});
                 console.log(`Atualizou a classificação do time: ${team}`);
             }
             else{
-                statistics['ano'] = 2019;
+                statistics['ano'] = program.year;
                 statistics['time'] = team;
                 Classificacao.create(statistics);
                 console.log(`Salvou o time: ${team}`);
@@ -45,6 +58,3 @@ async function init() {
         }
     }
 }
-
-
-init();
