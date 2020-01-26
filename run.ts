4782c85589aa38a62d2e1473a771c0cf781d3c0c
lohxx <lohanna.dev@gmail.com>
2020-01-26 16:09:16 -0300

    Corrige o retorno da função init

--- run.ts
+++ run.ts
@@ -1,18 +1,20 @@
 import * as moment from 'moment';
 import * as puppeteer from 'puppeteer';
 
+import { Round, ChampionshipData } from './types/types';
 import { RodadasCrawler } from './rodadas-crawler';
 import { ClassificacaoCrawler } from './classificacao-crawler';
 
 
-async function init() {
+async function init(): Promise<[Round[], ChampionshipData]> {
     const pupuppeteer = await puppeteer.launch({headless: false, devtools: true});
     const currentYear =  moment().get('year');
     const classificacaoCrawler = new ClassificacaoCrawler(currentYear, pupuppeteer);
     const rodadasCrawler = new RodadasCrawler(currentYear, pupuppeteer);
 
-    const t = await rodadasCrawler.initRoundExtraction();
-    return t;
+    const roundMatches = await rodadasCrawler.initRoundExtraction();
+    const teamsClassifications = await classificacaoCrawler.init();
+    return [roundMatches, teamsClassifications];
 }
 
 init().then(e => console.log(e));
